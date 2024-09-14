48b36a55806a332774ee62b84752c3dfac379ca9
tomocy <tomocy.dev@gmail.com>
2024-09-15 01:11:27 +0900

    Use ESLint Stylistic to format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,38 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import stylistic from '@stylistic/eslint-plugin';
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
+export default [
+    { files: ['**/*.ts'] },
+    { ignores: ['dist/*'] },
+
+    {
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+        },
+
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2022,
+            sourceType: 'module',
+        },
+
+        rules: {
+            '@typescript-eslint/naming-convention': ['warn', {
+                selector: 'import',
+                format: ['camelCase', 'PascalCase'],
+            }],
+
+            'curly': 'warn',
+            'eqeqeq': 'warn',
+            'no-throw-literal': 'warn',
+            'semi': 'warn',
+        },
     },
 
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-    },
-
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+    stylistic.configs.customize({
+        indent: 4,
+        quotes: 'single',
+        semi: true,
+    }),
+];
