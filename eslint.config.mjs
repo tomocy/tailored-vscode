4691bf027f92da76a6a9c04513b82003746c50c0
tomocy <tomocy.dev@gmail.com>
2024-09-16 17:57:00 +0900

    Update {TS, ESLint} configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,17 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import stylistic from '@stylistic/eslint-plugin';
+import tsParser from '@typescript-eslint/parser'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
-    { files: ['**/*.ts'] },
-    { ignores: ['dist/*'] },
-
     {
-        plugins: {
-            '@typescript-eslint': typescriptEslint,
-        },
-
+        files: ['**/*.ts'],
         languageOptions: {
             parser: tsParser,
-            ecmaVersion: 2022,
-            sourceType: 'module',
-        },
-
-        rules: {
-            '@typescript-eslint/naming-convention': ['warn', {
-                selector: 'import',
-                format: ['camelCase', 'PascalCase'],
-            }],
-
-            'curly': 'warn',
-            'eqeqeq': 'warn',
-            'no-throw-literal': 'warn',
-            'semi': 'warn',
         },
     },
-
+    { ignores: ['dist/*'] },
     stylistic.configs.customize({
         indent: 4,
         quotes: 'single',
-        semi: true,
+        semi: false,
     }),
-];
+]
--- src/extension.ts
+++ src/extension.ts
@@ -1,67 +1,67 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
     {
         const disposable = vscode.commands.registerCommand('tailored-vscode.cursor.moveToPageStart', () => {
-            vscode.commands.executeCommand('cursorTop');
-        });
+            vscode.commands.executeCommand('cursorTop')
+        })
 
-        context.subscriptions.push(disposable);
+        context.subscriptions.push(disposable)
     }
     {
         const disposable = vscode.commands.registerCommand('tailored-vscode.cursor.moveToPageEnd', () => {
-            vscode.commands.executeCommand('cursorBottom');
-        });
+            vscode.commands.executeCommand('cursorBottom')
+        })
 
-        context.subscriptions.push(disposable);
+        context.subscriptions.push(disposable)
     }
     {
         const disposable = vscode.commands.registerCommand('tailored-vscode.cursor.moveUpByLine', (options: { n: number }) => {
             vscode.commands.executeCommand('cursorMove', {
                 to: 'up',
                 value: options.n,
-            });
-        });
+            })
+        })
 
-        context.subscriptions.push(disposable);
+        context.subscriptions.push(disposable)
     }
     {
         const disposable = vscode.commands.registerCommand('tailored-vscode.cursor.moveDownByLine', (options: { n: number }) => {
             vscode.commands.executeCommand('cursorMove', {
                 to: 'down',
                 value: options.n,
-            });
-        });
+            })
+        })
 
-        context.subscriptions.push(disposable);
+        context.subscriptions.push(disposable)
     }
     {
         const disposable = vscode.commands.registerCommand('tailored-vscode.cursor.moveToLineStart', () => {
-            vscode.commands.executeCommand('cursorHome');
-        });
+            vscode.commands.executeCommand('cursorHome')
+        })
 
-        context.subscriptions.push(disposable);
+        context.subscriptions.push(disposable)
     }
     {
         const disposable = vscode.commands.registerCommand('tailored-vscode.cursor.moveToLineEnd', () => {
-            vscode.commands.executeCommand('cursorEnd');
-        });
+            vscode.commands.executeCommand('cursorEnd')
+        })
 
-        context.subscriptions.push(disposable);
+        context.subscriptions.push(disposable)
     }
     {
         const disposable = vscode.commands.registerCommand('tailored-vscode.cursor.moveLeftByWord', () => {
-            vscode.commands.executeCommand('cursorWordLeft');
-        });
+            vscode.commands.executeCommand('cursorWordLeft')
+        })
 
-        context.subscriptions.push(disposable);
+        context.subscriptions.push(disposable)
     }
     {
         const disposable = vscode.commands.registerCommand('tailored-vscode.cursor.moveRightByWord', () => {
-            vscode.commands.executeCommand('cursorWordRight');
-        });
+            vscode.commands.executeCommand('cursorWordRight')
+        })
 
-        context.subscriptions.push(disposable);
+        context.subscriptions.push(disposable)
     }
 }
 
